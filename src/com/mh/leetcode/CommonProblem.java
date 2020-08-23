30d820336bd82de532d4371ccdfb287d7cd6be3a
Howie.Mai 中智_麦皓 <Howie.Mai@tyj365.com>
2020-08-23 21:47:02 +0800

    第679题和第201题

--- src/com/mh/leetcode/CommonProblem.java
+++ src/com/mh/leetcode/CommonProblem.java
@@ -1,7 +1,6 @@
 package com.mh.leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * ClassName：
@@ -16,10 +15,13 @@ public class CommonProblem {
         Long start = System.currentTimeMillis();
 //        commonProblem.reverse(123456);
 //        System.out.println(commonProblem.fib(45));
-        System.out.println(commonProblem.countSubstrings("aaaaa"));
+//        System.out.println(commonProblem.countSubstrings("aaaaa"));
+//        System.out.println(commonProblem.judgePoint24(new int[]{4, 7, 1, 8}));
+        System.out.println(commonProblem.rangeBitwiseAnd(1,7));
         Long end = System.currentTimeMillis();
         System.out.println(end - start);
     }
+
     /**
      * 7
      * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
@@ -32,10 +34,10 @@ public class CommonProblem {
         int ans = 0;
         while (x != 0) {
             int pop = x % 10;
-            if (ans > Integer.MAX_VALUE / 10 || (ans == Integer.MAX_VALUE / 10 && pop > 7)){
+            if (ans > Integer.MAX_VALUE / 10 || (ans == Integer.MAX_VALUE / 10 && pop > 7)) {
                 return 0;
             }
-            if (ans < Integer.MIN_VALUE / 10 || (ans == Integer.MIN_VALUE / 10 && pop < -8)){
+            if (ans < Integer.MIN_VALUE / 10 || (ans == Integer.MIN_VALUE / 10 && pop < -8)) {
                 return 0;
             }
             ans = ans * 10 + pop;
@@ -59,18 +61,18 @@ public class CommonProblem {
         // result[i][j] 表示字符串的下标为i到下标j的字符串是否为回文串
         // 0 : 不是 1 : 是
         int[][] result = new int[n][n];
-        for (int i = 0;i < n;i++){
-            for (int j = 0;j <= i;j++){
-                if(i == j){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (i == j) {
                     result[i][j] = 1;
                     ans++;
-                }else {
+                } else {
                     boolean isSame = s.charAt(i) == s.charAt(j);
-                    if(i - j == 1 && isSame){
+                    if (i - j == 1 && isSame) {
                         // 相邻字符相同也为回文串
                         result[i][j] = 1;
                         ans++;
-                    }else if(i - j > 1 && isSame && result[i-1][j+1] == 1){
+                    } else if (i - j > 1 && isSame && result[i - 1][j + 1] == 1) {
                         result[i][j] = 1;
                         ans++;
                     }
@@ -100,9 +102,108 @@ public class CommonProblem {
         int[] nums = new int[n + 1];
         nums[0] = 0;
         nums[1] = 1;
-        for(int i = 2;i <= n;i++){
-            nums[i] = (nums[i - 1] + nums [i - 2]) % 1000000007;
+        for (int i = 2; i <= n; i++) {
+            nums[i] = (nums[i - 1] + nums[i - 2]) % 1000000007;
         }
         return nums[n];
+    }
+
+    /**
+     * 679. 24 点游戏
+     * 你有 4 张写有 1 到 9 数字的牌。你需要判断是否能通过 *，/，+，-，(，) 的运算得到 24。
+     */
+    public boolean judgePoint24(int[] nums) {
+        //转成double数组计算
+        double[] doubles = Arrays.stream(nums).asDoubleStream().toArray();
+        return solve(doubles);
+    }
+
+    public static boolean solve(double[] nums) {
+        if(nums.length == 0){
+            return false;
+        }
+
+        //递归出口，剩余一个数，判断是不是24点
+        if (nums.length == 1) {
+            //处理计算精度问题
+            return nums[0] > 23.999 && nums[0] < 24.001;
+        }
+
+        //任意拿两个不相同的数，通过运算符组成第三个数，进行后续的24点计算
+        //其中，括号不用考虑，因为是任意两个数的所有允许符都参与，所以有括号，没括号的情况，都包含在内了
+        for (int x = 0; x < nums.length - 1; x++) {
+            for (int y = x + 1; y < nums.length; y++) {
+                if(x == 0 && y == 2){
+                    System.out.println("123");
+                }
+                //是否合法
+                boolean isValid;
+
+                //每次两数操作 出来的数组是原来数据的length-1
+                //删除后面的元素（这样不会影响前面的元素），前面的元素用来放置新值
+                double[] temp = new double[nums.length - 1];
+                // y为要和x进行加减乘除的数
+                //copy待删除元素y的前部
+                System.arraycopy(nums, 0, temp, 0, y);
+                //copy待删除元素y的后部
+                System.arraycopy(nums, y + 1, temp, y, temp.length - y);
+
+                //加法
+                temp[x] = nums[x] + nums[y];
+                //合法就返回
+                if (solve(temp)) {
+                    return true;
+                }
+
+                //减法
+                temp[x] = Math.abs(nums[x] - nums[y]);
+                //合法就返回
+                if (solve(temp)) {
+                    return true;
+                }
+
+                //乘法
+                temp[x] = nums[x] * nums[y];
+                //合法就返回
+                if (solve(temp)) {
+                    return true;
+                }
+
+                //除法，除与被除
+                if (nums[y] != 0) {
+                    temp[x] = nums[x] / nums[y];
+                    //合法就返回
+                    if (solve(temp)) {
+                        return true;
+                    }
+                }
+                if (nums[x] != 0) {
+                    temp[x] = nums[y] / nums[x];
+                    //合法就返回
+                    if (solve(temp)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        //不合法
+        return false;
+    }
+
+    /**
+     * 201. 数字范围按位与
+     * 给定范围 [m, n]，其中 0 <= m <= n <= 2147483647，返回此范围内所有数字的按位与（包含 m, n 两端点）。
+     */
+    public int rangeBitwiseAnd(int m, int n) {
+        int sum = 0;
+        // 比如从5(0101)到7(0111) 因为与是有0就为0，所以只要找相同的前缀，在范围数里面肯定有为0的，所以后面必定计算出为0
+        while (m != n){
+            // 通过右移动并赋值找出一样的前缀
+            m >>= 1;
+            n >>= 1;
+            sum++;
+        }
+        // 找到相同前缀后，再向左补位(即0)
+        return m << sum;
     }
 }
