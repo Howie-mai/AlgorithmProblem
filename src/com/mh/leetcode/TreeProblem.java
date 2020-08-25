0e610aa1efe1a717a681681fc48d3b75ba7958ca
howie.mai <howie.mai@tyj365.com>
2020-08-25 18:00:23 +0800

    add 第437题

--- src/com/mh/leetcode/TreeProblem.java
+++ src/com/mh/leetcode/TreeProblem.java
@@ -512,4 +512,35 @@ public class TreeProblem {
             preOrderByDiGui(child,list);
         }
     }
+
+    /**
+     * 437. 路径总和 III
+     * 给定一个二叉树，它的每个结点都存放着一个整数值。
+     * 找出路径和等于给定数值的路径总数。
+     * 路径不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
+     */
+    @SuppressWarnings("unused")
+//    int totalPath = 0;
+    public int pathSum(TreeNode root, int sum) {
+        if(root == null){
+            return 0;
+        }
+        Sum(root,sum);
+        pathSum(root.left,sum);
+        pathSum(root.right,sum);
+
+        return totalPath;
+    }
+
+    public void Sum(TreeNode root, int sum){
+        if(root == null){
+            return;
+        }
+        sum -= root.val;
+        if(sum == 0){
+            totalPath++;
+        }
+        Sum(root.left,sum);
+        Sum(root.right,sum);
+    }
 }
