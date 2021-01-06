434592f50c04d6e40246aa054c2ee45fab0f9a1c
howie.mai <howie.mai@tyj365.com>
2021-01-06 18:08:52 +0800

    add

--- src/com/mh/leetcode/GreedyProblem.java
+++ src/com/mh/leetcode/GreedyProblem.java
@@ -10,7 +10,6 @@ import java.util.*;
  * @author mh
  */
 public class GreedyProblem {
-
     public static void main(String[] args) {
         GreedyProblem greedyProblem = new GreedyProblem();
         int[] nums1 = new int[]{2, 5, 6, 4, 4, 0};
@@ -19,7 +18,7 @@ public class GreedyProblem {
 //        System.out.println(Arrays.toString(greedyProblem.getMaxSubsequence(new int[]{9, 1, 2, 5, 8, 3}, 1)));
 //        System.out.println(Arrays.toString(greedyProblem.merge(nums1, nums2, 15)));
 //        System.out.println(greedyProblem.removeKdigits("99899",1));
-        System.out.println(greedyProblem.monotoneIncreasingDigits(332));
+//        System.out.println(greedyProblem.monotoneIncreasingDigits(332));
     }
 
     /**
@@ -351,6 +350,30 @@ public class GreedyProblem {
             }
 
             ans += Math.max(left[i],right);
+        }
+        return ans;
+    }
+
+    /**
+     * 455. 分发饼干
+     * 假设你是一位很棒的家长，想要给你的孩子们一些小饼干。但是，每个孩子最多只能给一块饼干。
+     * 对每个孩子 i，都有一个胃口值 g[i]，这是能让孩子们满足胃口的饼干的最小尺寸；并且每块饼干 j，都有一个尺寸 s[j] 。
+     * 如果 s[j] >= g[i]，我们可以将这个饼干 j 分配给孩子 i ，这个孩子会得到满足。
+     * 你的目标是尽可能满足越多数量的孩子，并输出这个最大数值。
+     */
+    public int findContentChildren(int[] g, int[] s) {
+        int ans = 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int a = 0, b = 0;
+        while (a < g.length && b < s.length){
+            if(s[b] >= g[a]){
+                a++;
+                b++;
+                ans++;
+            }else if(g[a] > s[b]){
+                b++;
+            }
         }
         return ans;
     }
